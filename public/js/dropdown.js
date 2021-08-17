636ae3e2dc5216f6c9b969f7605e488235c4eb2f
ouail-nazim <ouailnazim20@gmail.com>
2021-08-17 21:29:39 +0100

    add dropdowns menu

---
+++ public/js/dropdown.js
@@ -0,0 +1,18 @@
+window.addEventListener('click',e=>{
+    if(["menu-button","menu-button-content"].indexOf(e.target.getAttribute('role')) === -1){
+        if (e.target.getAttribute('role')!=="menu") {
+            document.querySelectorAll('[role="menu"]').forEach(menu => {
+                menu.classList.add('hidden');
+            })
+        }
+    }
+    
+})
+Array.from(document.querySelectorAll('[role="menu-button"]')).forEach(menu_button => {
+    menu_button.addEventListener('click',()=>{
+        document.querySelectorAll('[role="menu"]').forEach(menu => {
+                menu.classList.add('hidden');
+        })
+        menu_button.nextElementSibling.classList.toggle("hidden")
+    })
+});
